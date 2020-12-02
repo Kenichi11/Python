bc840510990b1b5370f284171c422f0e3bf33f4f
Kenichi <72405331+MarcMelendez1@users.noreply.github.com>
2020-12-02 10:04:36 -0800

    Create functionDemo.py

---
+++ 2021/functionDemo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# functionDemo.py
+def printStars(n):
+	print(n," ",end="")
+	print("* * * * * * * * * * * * *")
+	print("* * * * * * * * * * * * *")
+	print("* * * * * * * * * * * * *")
+	
+def main():
+	printStars(1)# Calling a funtion printStars(n)
+	printStars(2)
+	printStars(3)
+main()
+
+
+'''
+python functions
+- defining a function
+- calling a function 
+- function arguments (parameters)
+'''
