8f5b50aeb01fdfb2da159cc3c8526668becbf5e0
Kenichi <72405331+MarcMelendez1@users.noreply.github.com>
2020-12-02 10:15:35 -0800

    Create variables-1.py

---
+++ 2021/variables-1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#python3 variables-1.py owo
+#Variables
+i1 - 33 #integer 1
+i2 = 48
+i3 = 65
+i4 = i3+25
+i5 = 97
+#output
+print ("integers")
+print(i1,i2,i3,i4,i5)# debug
+#type cast variables
+c1 = chr(i1)
+c2 = chr(i2)
+c3 = chr(i3)
+c4 = chr(i4)
+c5 = chr(i5)
+print("characters")
+print(c1,c2,c3,c4,c5)
+# Add the variables 
+sum = i1+i2+i3+i4+i5
+print("Sum: ",sum)
+print(type(sum)
+# * * * * * * *
+concat = c1+c2+c3+c4+c5
+print("This is now a string: ",concat)
+print(type(concat))
